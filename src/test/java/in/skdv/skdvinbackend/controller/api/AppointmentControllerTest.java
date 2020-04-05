dd5f9772029bee34367c75e881dfc0edafe07442
Andreas Mueller <andreas.mueller@yaga.eu>
2020-04-05 10:40:45 +0200

    add tests

--- src/test/java/in/skdv/skdvinbackend/controller/api/AppointmentControllerTest.java
+++ src/test/java/in/skdv/skdvinbackend/controller/api/AppointmentControllerTest.java
@@ -421,6 +421,23 @@ public class AppointmentControllerTest extends AbstractSkdvinTest {
 
 
     @Test
+    public void testAddAdminAppointment_Error() throws Exception {
+        Appointment appointment = ModelMockHelper.createSingleAppointment();
+        appointment.setPicAndVid(5);
+        String appointmentJson = json(appointment);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.post("/api/appointment/admin")
+                .header("Accept-Language", "en-US")
+                .header("Authorization", MockJwtDecoder.addHeader(CREATE_APPOINTMENTS))
+                .contentType(contentType)
+                .content(appointmentJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect(jsonPath("$.message", is("The appointment has more video than tandem slots")));
+    }
+
+
+    @Test
     public void testUpdateAppointment() throws Exception {
         AppointmentConverter appointmentConverter = new AppointmentConverter();
         AppointmentDTO appointment = appointmentConverter.convertToDto(appointmentService.findAppointmentsByDay(LocalDate.now()).get(0));
@@ -443,7 +460,7 @@ public class AppointmentControllerTest extends AbstractSkdvinTest {
                 .andExpect(jsonPath("$.payload.tandem", is(newCount)))
                 .andExpect(jsonPath("$.payload.customer.firstName", is("Unitjane")))
                 .andExpect(jsonPath("$.payload.customer.jumpers", hasSize(2)))
-                .andDo(document("appointment/update-appointment",
+                .andDo(document("appointment/update-admin-appointment",
                         requestFields(
                                 fieldWithPath("appointmentId").description("The appointment's id"),
                                 fieldWithPath("customer").description("Customer Details"),
@@ -571,6 +588,22 @@ public class AppointmentControllerTest extends AbstractSkdvinTest {
                                 fieldWithPath("payload.createdBy").ignored(),
                                 fieldWithPath("payload.clientId").ignored()
                         )));
+    }
+
+    @Test
+    public void testUpdateAdminAppointment_Error() throws Exception {
+        Appointment appointment = ModelMockHelper.createSingleAppointment();
+        appointment.setAppointmentId(99999999);
+        String appointmentJson = json(appointment);
+
+        mockMvc.perform(RestDocumentationRequestBuilders.put("/api/appointment/admin")
+                .header("Accept-Language", "en-US")
+                .header("Authorization", MockJwtDecoder.addHeader(UPDATE_APPOINTMENTS))
+                .contentType(contentType)
+                .content(appointmentJson))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect(jsonPath("$.message", is("Appointment not found")));
     }
 
     @Test
