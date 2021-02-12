bddd1389774c39844d4fb34df6448a661786c4b4
Andreas Mueller <andreas.mueller@yaga.eu>
2021-02-12 16:22:37 +0100

    #158 fix appointment reminder for admin appointments

---
+++ src/main/java/in/skdv/skdvinbackend/model/entity/settings/Tandemwaiver.java
@@ -0,0 +1,14 @@
+package in.skdv.skdvinbackend.model.entity.settings;
+
+public class Tandemwaiver {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
