973d3cbff1e2429a41cb080f233df5bac53654bf
Andreas Mueller <andreas.mueller@yaga.eu>
2023-04-14 20:50:54 +0200

    upgrade springdoc from v1 to v2

--- src/main/java/in/skdv/skdvinbackend/model/entity/OutgoingMail.java
+++ src/main/java/in/skdv/skdvinbackend/model/entity/OutgoingMail.java
@@ -2,7 +2,7 @@ package in.skdv.skdvinbackend.model.entity;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import nonapi.io.github.classgraph.json.Id;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
